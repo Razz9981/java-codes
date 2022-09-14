92223e11d72635726db7f74e4e8847aba9a20ce2
Razz9981 <Rajkharerdk@gmail.com>
2022-09-14 18:11:40 +0100

    Initial commit

--- MarJava.java
+++ MarJava.java
@@ -1,3 +1,37 @@
-// git remote add origin https://github.com/Razz9981/java-codes.git
-// git branch -M main
-// git push -u origin main
+import java.util.Scanner;
+
+public class MarJava {
+    public static float a, b;
+    static char ch;
+
+    public void calculate() {
+        if (ch == '*') {
+            System.out.println(a * b);
+        } else if (ch == '+') {
+            System.out.println(a + b);
+        } else if (ch == '/') {
+            System.out.println(a / b);
+        } else if (ch == '-') {
+            System.out.println(a - b);
+        } else {
+            System.out.println("Enter a valid Operator");
+        }
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the First value :");
+            a = sc.nextInt();
+
+            System.out.println("Enter the second value :");
+            b = sc.nextInt();
+
+            System.out.println("Enter the operatio :'+','-','*','/'");
+            ch = sc.next().charAt(0);
+        }
+
+        MarJava m = new MarJava();
+        m.calculate();
+
+    }
+}
